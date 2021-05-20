5535e13714bf93927be6f409b9d9992d5bd644c7
oreoluwade <oreoluwade@gmail.com>
2021-05-20 18:12:19 +0100

    Setup linting, styling

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.scss';
+
+const Home = () => {
+  return <div className={styles.container}>Default Home page</div>;
+};
+
+export default Home;
