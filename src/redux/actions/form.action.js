091aa1fc4451b0a26ffab6bf26788a43c1468f8d
oreoluwade <oreoluwade@gmail.com>
2021-05-21 11:03:02 +0100

    Add action for setting selected Tab

--- src/redux/actions/form.action.js
+++ src/redux/actions/form.action.js
@@ -1,8 +1,15 @@
-import { SET_FORM_STEP } from '../../constants';
+import { SET_FORM_STEP, SET_SELECTED_TAB } from '../../constants';
 
 export const setFormStep = data => {
   return {
     type: SET_FORM_STEP,
+    data
+  };
+};
+
+export const setSelectedTab = data => {
+  return {
+    type: SET_SELECTED_TAB,
     data
   };
 };
--- src/redux/reducers/form.reducer.js
+++ src/redux/reducers/form.reducer.js
@@ -1,10 +1,13 @@
-import { SET_FORM_STEP } from '../../constants';
+import { SET_FORM_STEP, SET_SELECTED_TAB } from '../../constants';
 import initialState from '../../store/initial-state';
 
 const formReducer = (state = initialState.form, action) => {
   switch (action.type) {
     case SET_FORM_STEP:
       return { ...state, step: action.data };
+
+    case SET_SELECTED_TAB:
+      return { ...state, selectedTab: action.data };
 
     default:
       return { ...state };
